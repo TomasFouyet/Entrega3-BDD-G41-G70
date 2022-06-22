eb9f3e413e5f3f03498ee06280445da9b2eb05fc
Jesus Delgado <jgdelgado@uc.cl>
2022-06-22 17:10:13 -0400

    Index actualizado

--- Sites/config/data.php
+++ Sites/config/data.php
@@ -3,7 +3,8 @@
     $user_70 = 'grupo70';
     $password_70 = 'grupo70';
     $db_name_70 = 'grupo70e3';
-    $user_41 = ''
-    $password_41 = ''
-    $db_name_41 = ''
+    
+    $user_41 = 'grupo41';
+    $password_41 = 'datosgrupo41';
+    $db_name_41 = 'grupo41e2';
 ?>
--- Sites/index.php
+++ Sites/index.php
@@ -1,71 +1,64 @@
 <html>
 <!DOCTYPE html>
 <html class="has-background-grey-lighter">
-<style>
-ul {list-style-type: none;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-  background-color: #333;}
 
-li {float: left;}
-
-li a {display: block;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;}
-
-.clase {float: right}
-
-li a:hover {background-color: #111;}
-</style>
 <head>
-  <title>Grupo 41 - 70 // BDD entrega 3</title>
-  
+  <title>Grupo 41 - 70 // BDD entrega 32</title>
 </head>
+
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Parallax - Free Bulma template</title>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;600;800&display=swap" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/15181efa86.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://unpkg.com/bulma@0.9.0/css/bulma.min.css" />
     <link rel="stylesheet" type="text/css" href="../css/hello-parallax.css">
 </head>
-    <body align=center>
-    <nav class="navbar" role="navigation" aria-label="main navigation" >
+
+
+<body align=center>
+
+    <nav class="navbar" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
-        <a class="navbar-item" href="https://dcc.ing.puc.cl/">
-          <img id="logo" src="https://postgradosdcc.ing.puc.cl/hosted/images/db/2a63921bc04365b0724de5715d4807/OG-DCC.jpg" width="112" height="60">
+        <a class="navbar-item">
+          <img id="logo" src="https://postgradosdcc.ing.puc.cl/hosted/images/db/2a63921bc04365b0724de5715d4807/OG-DCC.jpg" width="112" height="28">
         </a>
-    
+
         <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
+
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+        <!-- Inicio de la navbar -->
+        </div>
+
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <a class="button is-primary" href="/~grupo41/login.php">
+                <strong>Iniciar Sesión</strong>
+              </a>
+              <a class="button is-light" href="/funcionalidades/importar_usuarios.php">
+                Importar Usuarios
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+
     <?php if (isset($_SESSION['user_name'])) { ?>
       <h2 class="title is-1"> Hola <?php echo $_SESSION['user_name'] ?>>
         <form class="buttons" action="/~grupo41/logout.php">
           <input class="button" type="submit" value="Cerrar Session">
         </form>
     <?php } else { ?>
-      <div id="navbarBasicExample" class="navbar-menu">
-        <div class="navbar-start">
-          <a class="navbar-item" href="/~grupo41/login.php">
-            Iniciar Sesion
-          </a>
 
-          <a class="navbar-item" href="/~grupo41/funcionalidades/importar_usuarios.php">
-            Importar Usuarios
-          </a>
-        </div>
-      </div>
+
     <?php } ?>
-    </nav>
+
+
+
+
+
     <section class="hero is-medium">
       <div class="hero-body">
         <div class="container">
@@ -91,3 +84,4 @@ li a:hover {background-color: #111;}
     </section>
     
   </body>
+  <img id="logo" src="https://postgradosdcc.ing.puc.cl/hosted/images/db/2a63921bc04365b0724de5715d4807/OG-DCC.jpg" width="112" height="60">
