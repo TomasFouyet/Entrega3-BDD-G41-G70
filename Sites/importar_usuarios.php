f005280c962a92f757eeab9872bfa08533cef3f2
TOMAS FOUYET <tfouyet@uc.cl>
2022-06-24 08:04:37 -0400

    Cambio en importar_usuarios.php

--- Sites/importar_usuarios.php
+++ Sites/importar_usuarios.php
@@ -2,51 +2,60 @@
     // Esta parte lo que hace es importar la funcion q crea el admin 
     require("config/conection.php");
 
-    $query = "SELECT crear_admin_DGAC();";
+    $query = "INSERT INTO Usuarios VALUES(999, 'DGAC', 'admin', 'Admin DGAC');";
 
     $result = $db2 -> prepare($query);
     $result -> execute();
-    // $personas = $result -> fetchAll();
+    //  $personas = $result -> fetchAll();
 
     // Esta parte nos entrega las compañias
-    $query = "SELECT * FROM vuelos;";
+    $query2 = "SELECT * FROM vuelos;";
+    $result2 = $db2 -> prepare($query2);
 
-    $result -> execute();
-    $companias_aereas = $result -> fetchAll();
+    $result2 -> execute();
+    $companias_aereas = $result2 -> fetchAll();
 
     foreach ($companias_aereas as $compania){
-        $query = "SELECT crear_compania_usuario('$compania[0]', '$compania[5]');";
+        $query3 = "INSERT INTO Usuarios VALUES('$compania[0]', '$compania[5]', '123456', 'Compania Aerea');";
 
-        $result = $db2 -> prepare($query);
-        $result -> execute();
-        $result -> fetchAll();
+        $result3 = $db2 -> prepare($query3);
+        $result3 -> execute();
+        $result3 -> fetchAll();
     }
 
     // Esta parte nos entrega las persona/pasajeros
 
-    $query = "SELECT * FROM pasajeros;";
+    $query4 = "SELECT * FROM pasajeros;";
     
-    $result -> execute();
-    $pasajeros = $result -> fetchAll();
+    $result4 -> execute();
+    $pasajeros = $result4 -> fetchAll();
 
     foreach ($pasajeros as $pasajero){
-        $query = "SELECT crear_pasajero_usuario('$pasajero[0]', '$pasajero[3]');";
+        $query5 = "INSERT INTO Usuarios VALUES('$pasajero[0]', '$pasajero[3]', '123456', 'Pasajero');";
 
-        $result = $db2 -> prepare($query);
-        $result -> execute();
-        $result -> fetchAll();
+        $result5 = $db2 -> prepare($query5);
+        $result5 -> execute();
+        $result5 -> fetchAll();
+
     }
 ?>
 
+<?php
+    $query6 = "SELECT * FROM Usuarios;";
+    $result6 = $db2 -> prepare($query6);
+
+    $result6 -> execute();
+    $usuarios = $result6 -> fetchAll();
+?>
 <table align='center' class="table is-striped">
     <tr>
         <th>Id de usuario</th>
-        <th>Nombre</th>
-        <th>Pasaporte</th>
+        <th>Username</th>
+        <th>Tipo de Usuario</th>
     </tr>
     <?php
-    foreach($personas as $persona){
-        echo "<tr><td>$persona[0]</td><td>$persona[1]</td><td>$persona[2]</td></tr>";
+    foreach($usuarios as $usuario){
+        echo "<tr><td>$usuario[0]</td><td>$usuario[1]</td><td>$usuario[3]</td></tr>";
     }
     ?>
 </table>
