a137081caff7bf79307bfcb069ab8faa11086c02
TOMAS FOUYET <tfouyet@uc.cl>
2022-06-25 16:14:15 -0400

    navegacion compañia

--- Sites/navegacion_compania.php
+++ Sites/navegacion_compania.php
@@ -9,28 +9,7 @@
 
 
 ?>
-<!-- Esto de Aqui todavia no esta funcionando, tenemos que ver como hacerlo funcionar -->
-<h2 align="center"> Buscar Vuelo por fechas:</h2>
-  <form align="center" action="consultas/consulta_vuelos_fecha.php" method="post">
-    Ingrese las fechas:
-    <input type="date" name="fecha1"> a <input type="date" name="fecha2">
-    <br>
-    <input button class="button is-link" type="submit" value="Buscar">
-    <br/><br/>
-</form>
-<?php
-    require("../config/conexion.php"); 
-    $fecha1 = $_POST["fecha1"];
-    $fecha2 = $_POST["fecha2"];
-    $query2 = "SELECT * FROM tipo_vuelo AS tv, propuesta_de_vuelo AS pdv
-               WHERE tv.fecha_salida >= '$fecha1' AND tv.fecha_llegada <= '$fecha2';";
-    $result2 = $db -> prepare($query2);
-    $result2 -> execute();
-    $viajes = $result2 -> fetchAll();
-?>
-
-
-
+<h3 align="center">Vuelos Aceptados</h3>
 <table align='center' class="table is-striped">
     <tr>
         <th>Codigo De Vuelo</th>
@@ -43,7 +22,35 @@
     </tr>
     <?php
     foreach($propuestas as $propuesta){
-        if($propuesta[0] == 'pendiente') {
+        if($propuesta[0] == 'aceptado') {
+            echo "<tr>
+                    <td>$propuesta[1]</td>
+                    <td>$propuesta[9]</td>
+                    <td>$propuesta[0]</td>
+                    <td>$propuesta[2]</td>
+                    <td>$propuesta[3]</td>
+                    <td>$propuesta[4]</td>
+                    <td>$propuesta[5]</td>
+                </tr>";
+        }
+    }
+    ?>
+</table>
+
+<h3 align="center">Vuelos Rechazados</h3>
+<table align='center' class="table is-striped">
+    <tr>
+        <th>Codigo De Vuelo</th>
+        <th>Codigo De La Compañia</th>
+        <th>Estado Del Vuelo</th>
+        <th>Fecha Salida</th>
+        <th>Fecha Llegada</th>
+        <th>Id aerodromo salida</th>
+        <th>Id aerodromo llegada</th>
+    </tr>
+    <?php
+    foreach($propuestas as $propuesta){
+        if($propuesta[0] == 'rechazado') {
             echo "<tr>
                     <td>$propuesta[1]</td>
                     <td>$propuesta[9]</td>
